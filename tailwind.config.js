25600cd9b7922192f6a367f4c0d41b5e814fe354
Davi Viana <davianasales@outlook.com>
2023-01-22 23:34:18 -0300

    chore: add project files

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,vue,jsx}"],
+  theme: {
+    extend: {},
+  },
+  prefix: "w-",
+  plugins: [],
+};
