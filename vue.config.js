413f49da00f0e2599e5f8b2f42d4f0e7f114b46f
Davi Viana <davianasales@outlook.com>
2023-01-24 22:48:38 -0300

    build: add jetpack, disable polyfills

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+    },
+  },
+  configureWebpack: {}
 });
